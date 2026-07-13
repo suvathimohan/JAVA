ae19916c3130793864b026260e74f99d09990825
SuvathiMohan <swathimohan31306@gmail.com>
2026-07-13 11:30:29 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
+
 class Solution {
     public int reverse(int x) {
-        int temp = x;
-        int rev = 0;
-        while(x>0 || x<0){
-            int ld = x%10;
-            x/=10;
-            rev = rev*10+ld;
+        long rev = 0;
+        while (x != 0) {
+            int ld = x % 10;
+            x /= 10;
+            rev = rev * 10 + ld;
         }
-        return rev;
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) rev;
     }
 }
-public class Main{
-    public static void main(String[] args){
+
+public class Main {
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
+
         Solution nw = new Solution();
-        nw.reverse(n);
+
+        System.out.println(nw.reverse(n));
     }
 }
